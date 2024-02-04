0768672a2c13ef5caa65e13349e6be2ac60bda7c
Luis Abeler <la@abeler-online.de>
2024-02-04 23:50:17 +0100

    New extensions

--- lib/ui.dart
+++ lib/ui.dart
@@ -3,4 +3,12 @@ import 'package:flutter/material.dart';
 extension HugoTextX on Text {
   Text get bold =>
       Text(data ?? "", style: const TextStyle(fontWeight: FontWeight.bold));
+  Text get larger =>
+      Text(data ?? "", style: TextStyle(fontSize: (style?.fontSize ?? 18) + 2));
+}
+
+extension HugoBuildContextX on BuildContext {
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
+  Color get primaryColor => colorScheme.primary;
+  Color get secondaryColor => colorScheme.secondary;
 }
