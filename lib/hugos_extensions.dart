cdbff0e4c302517540662ece172a03079b4d13cf
Luis Abeler <la@abeler-online.de>
2024-02-04 23:34:57 +0100

    some restructuring

--- lib/hugos_extensions.dart
+++ lib/hugos_extensions.dart
@@ -1,8 +1,3 @@
 library hugos_extensions;
 
-import 'package:flutter/material.dart';
-
-extension HugoTextX on Text {
-  Text get bold =>
-      Text(data ?? "", style: const TextStyle(fontWeight: FontWeight.bold));
-}
+export 'ui.dart';
---
+++ lib/ui.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+extension HugoTextX on Text {
+  Text get bold =>
+      Text(data ?? "", style: const TextStyle(fontWeight: FontWeight.bold));
+}
